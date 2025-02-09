97e542097faaf52815bbc8f63d06e63d661b8cce
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-02-09 19:53:00 +0200

    edit view ui part 3

--- lib/features/products/view/edit_product.dart
+++ lib/features/products/view/edit_product.dart
@@ -4,9 +4,21 @@ import 'package:our_market_admin/core/components/custom_elevated_button.dart';
 import 'package:our_market_admin/core/components/custom_text_field.dart';
 import 'package:our_market_admin/core/functions/build_custom_app_bar.dart';
 
-class EditProductView extends StatelessWidget {
+class EditProductView extends StatefulWidget {
   const EditProductView({super.key});
 
+  @override
+  State<EditProductView> createState() => _EditProductViewState();
+}
+
+class _EditProductViewState extends State<EditProductView> {
+  String selectedValue = "Collections";
+  String sale = "10";
+  final TextEditingController _productNameController = TextEditingController();
+  final TextEditingController _newPriceController = TextEditingController();
+  final TextEditingController _oldPriceController = TextEditingController();
+  final TextEditingController _productDescriptionController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,34 +30,55 @@ class EditProductView extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                DropdownMenu(
+                  initialSelection: selectedValue,
+                  onSelected: (String? value) {
+                    setState(() {
+                      selectedValue = value ?? "Collections";
+                      print(selectedValue);
+                    });
+                  },
+                  dropdownMenuEntries: const [
+                    DropdownMenuEntry(value: "Sports", label: "Sports"),
+                    DropdownMenuEntry(
+                        value: "Electronics", label: "Electronics"),
+                    DropdownMenuEntry(
+                        value: "Collections", label: "Collections"),
+                    DropdownMenuEntry(value: "Books", label: "Books"),
+                    DropdownMenuEntry(value: "Bikes", label: "Bikes")
+                  ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
                 Column(
                   children: [
                     Text("Sale"),
                     SizedBox(
                       height: 10,
                     ),
-                    Text("10 %")
+                    Text("$sale %")
                   ],
                 ),
                 Column(
                   children: [
-                    CacheImage(
+                    const CacheImage(
                         url:
                             "https://img.freepik.com/free-photo/sale-with-special-discount-vr-glasses_23-2150040380.jpg?t=st=1739116086~exp=1739119686~hmac=50674df6ab1e31c30ae312456d3292a4b517ea9c9d913f8e4d1c0728052f310f&w=900",
                         height: 200,
                         width: 300),
-                    SizedBox(
+                    const SizedBox(
                       height: 10,
                     ),
                     Row(
                       children: [
                         CustomElevatedButton(
-                            child: Icon(Icons.image), onPressed: () {}),
-                        SizedBox(
+                            child: const Icon(Icons.image), onPressed: () {}),
+                        const SizedBox(
                           width: 10,
                         ),
                         CustomElevatedButton(
-                            child: Icon(Icons.upload_file_rounded),
+                            child: const Icon(Icons.upload_file_rounded),
                             onPressed: () {})
                       ],
                     )
@@ -53,29 +86,58 @@ class EditProductView extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 60,
             ),
-            CustomField(labelText: "Product Name"),
-            SizedBox(
+            CustomField(
+              labelText: "Product Name",
+              controller: _productNameController,
+            ),
+            const SizedBox(
               height: 10,
             ),
-            CustomField(labelText: "New Price"),
-            SizedBox(
+            CustomField(
+              labelText: "New Price",
+              controller: _newPriceController,
+            ),
+            const SizedBox(
               height: 10,
             ),
-            CustomField(labelText: "Old Price"),
-            SizedBox(
+            CustomField(
+              labelText: "Old Price",
+              controller: _oldPriceController,
+            ),
+            const SizedBox(
               height: 10,
             ),
-            CustomField(labelText: "Product Description"),
-            SizedBox(
+            CustomField(
+              labelText: "Product Description",
+              controller: _productDescriptionController,
+            ),
+            const SizedBox(
               height: 20,
             ),
-            CustomElevatedButton(child: const Text("Update"), onPressed: () {})
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: CustomElevatedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Text("Update"),
+                  ),
+                  onPressed: () {}),
+            )
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _productNameController.dispose();
+    _newPriceController.dispose();
+    _oldPriceController.dispose();
+    _productDescriptionController.dispose();
+    super.dispose();
   }
 }
